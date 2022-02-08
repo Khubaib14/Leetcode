f61645c52b62b486af891095105e368b087bb178
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-02-08 22:22:03 +0530

    Time: 60 ms (83.65%), Space: 14.1 MB (88.27%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,15 +1,7 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        # break into two
-        lst1 = nums[:n]
-        lst2 = nums[n:]
-
-        # make new empty list
-        nums = []
-
-        # run a loop, taking same index from each
-        for i in range(n):
-            nums.append(lst1[i])
-            nums.append(lst2[i])
-
-        return nums
+        l = []    
+        for i, j in zip(nums[:n], nums[n:]):
+            l.append(i)
+            l.append(j)
+        return l
