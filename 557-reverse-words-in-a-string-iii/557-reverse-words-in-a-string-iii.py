f06aa6042f57d1f3e908c6f9e4eb214d2726295c
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-02-18 17:29:48 +0530

    Time: 105 ms (20.51%), Space: 14.6 MB (75.94%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
@@ -1,5 +1,25 @@
 class Solution:
     def reverseWords(self, s: str) -> str:    
+        # so that last word also is reversed
+        s = s + " "
+        l,r = 0, 0
+
+        string = ""
+
+        while r < len(s):
+            if s[r] != " ":
+                r += 1
+            elif s[r] == " ":
+                string = string + s[l:r][::-1]
+                string = string + " "
+                r += 1
+                l = r
+        
+        # extra space appended being removed here
+        return string[:-1]
+
+       
+    def personal():   
         def reverser(s):
             """reverses string"""
             q = list(s)
