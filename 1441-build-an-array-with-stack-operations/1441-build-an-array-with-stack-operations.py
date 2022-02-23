456197255e946963d48a4aaefe4643bae8f00a7c
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-02-23 16:21:48 +0530

    Time: 41 ms (62.52%), Space: 13.9 MB (87.42%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
@@ -1,5 +1,21 @@
 class Solution:
     def buildArray(self, target: List[int], n: int) -> List[str]:
+        point = 0
+        num = 1
+        q = []
+        while num <= target[-1] or point < len(target):
+            if num == target[point]:
+                q.append("Push")
+                point += 1
+                num += 1
+            elif num != target[point]:
+                q.append("Push")
+                q.append("Pop")
+                num += 1    
+        return q
+        
+        
+    def solution_one(self, target: List[int], n: int):
         l = []
         for i in range(1,target[-1]+1):
             if i in target:
