dd95378290f86768589d21b0f1c4d0d184cd1b2d
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-02-25 12:34:09 +0530

    Time: 48 ms (57.52%), Space: 13.8 MB (85.14%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -3,7 +3,7 @@ class Solution:
         stack = []
     
         for i in s:
-            if len(stack) > 0 and ((ord(i) - ord(stack[-1]) == 32) or (ord(stack[-1]) - ord(i) == 32)):
+            if stack and ((ord(i) - ord(stack[-1]) == 32) or (ord(stack[-1]) - ord(i) == 32)):
                 stack.pop()
             else:
                 stack.append(i)
