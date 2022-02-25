059ee60d21bcd8d93f5d16678f8dce052e331517
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-02-25 17:29:28 +0530

    Time: 46 ms (44.83%), Space: 13.9 MB (58.98%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
@@ -1,5 +1,23 @@
 class Solution:
     def maxDepth(self, s: str) -> int:
+        pointer = 0
+        count = 0
+        maximum = 0
+        while pointer < len(s):
+            if s[pointer] == "(":
+                count += 1
+                maximum = max(count, maximum)
+            elif s[pointer] == ")":
+                count -= 1
+            pointer += 1
+
+        return maximum
+        
+        
+        
+        
+        
+        
         stack, count = [], 0
         for i in s:
             if i == "(":
