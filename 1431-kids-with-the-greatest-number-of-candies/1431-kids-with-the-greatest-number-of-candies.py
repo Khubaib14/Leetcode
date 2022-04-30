845b53f730c7d08b1978215838f9e127a4601aa2
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-05-01 00:30:51 +0530

    Time: 51 ms (55.92%), Space: 13.9 MB (17.40%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
@@ -8,12 +8,30 @@ class Solution:
             if i > maxtemp:
                 maxtemp = i
 
+        barrier = maxtemp - extraCandies
+        
+        for i in candies:
+            if i>= barrier:
+                result.append(True)
+            else:
+                result.append(False)
+
+        return result   
+        
+        
+    def Sol1(self, candies: List[int], extraCandies: int) -> List[bool]:       
+        maxtemp = 0
+        temp = 0
+        result = []
+
+        for i in candies:
+            if i > maxtemp:
+                maxtemp = i
+
         for i in candies:
             if (i + extraCandies) >= maxtemp:
                 result.append(True)
             else:
                 result.append(False)
-            # print(i, extraCandies)
 
-        # print(result)
         return result
