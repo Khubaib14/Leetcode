604cfa544308fe1968e3852ee74eb4b84fdc2b8b
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-05-02 02:45:12 +0530

    Time: 92 ms (46.22%), Space: 14.5 MB (32.64%) - LeetHub

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.py
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.py
@@ -1,6 +1,19 @@
 class Solution:
     def decompressRLElist(self, nums: List[int]) -> List[int]:
         result = []
+        for i in range(1, len(nums), 2):
+            result += [nums[i]]*nums[i-1]
+        return result
+        
+        
+        
+        
+        
+        
+        
+        
+    def SolOne(self, nums):
+        result = []
         times = 0
         
         for i in range(len(nums)):
