8595aa0559c31a1bf2552aefc6362dda02496a87
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-05-03 01:28:34 +0530

    Time: 1575 ms (5.01%), Space: 28 MB (35.50%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -1,5 +1,26 @@
 class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
+    def findDuplicate(self, s: List[int]) -> int:
+        i = 0
+
+        # swap sort processing
+        # returns swap sorted string s
+        while i < len(s):
+            if s[i] != s[s[i] - 1]:
+                s[s[i] - 1], s[i] = s[i], s[s[i] - 1]
+            else:
+                i += 1
+        # observation on swap sorted s
+        for i in range(len(s)):
+            if s[i] != (i+1):
+                return (s[i])
+        
+        
+        
+        
+        
+        
+        
+    def solOne(nums):    
         fast = 0
         d = {}
         while fast < len(nums):
