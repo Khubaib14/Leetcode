9aa624dfbeba80c4f8672daf809da43d80bc258a
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-05-06 22:28:04 +0530

    Time: 571 ms (5.01%), Space: 16.4 MB (18.69%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
@@ -1,5 +1,28 @@
 class Solution:
     def sortedSquares(self, s: List[int]) -> List[int]:
+        
+        result = [0]*len(s)
+
+        slow = 0
+        fast  = len(s) - 1
+        append_pointer = len(s) - 1
+
+        for i in range(len(s)):
+            slow_square = s[slow]**2
+            fast_square = s[fast]**2
+            if slow_square >= fast_square:
+                result[append_pointer] = slow_square
+                slow += 1
+            else:
+                result[append_pointer] = fast_square
+                fast -= 1
+            append_pointer -= 1
+        
+        return (result)
+        
+        
+        
+    def solTwo(self, s: List[int]) -> List[int]:
         result = []
 
         slow = 0
@@ -17,11 +40,7 @@ class Solution:
         
         return (result)
         
-        
-        
-        
-        
-        
+
         
     def solOne(self, s: List[int]) -> List[int]:
         slow = 0
