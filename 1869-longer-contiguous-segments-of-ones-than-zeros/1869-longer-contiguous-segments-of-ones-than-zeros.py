7bcf13bacbfefa28de7f084007a1b2428e5692bf
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-05-10 15:10:37 +0530

    Time: 20 ms (99.78%), Space: 13.9 MB (16.88%) - LeetHub

--- 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.py
+++ 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.py
@@ -6,11 +6,12 @@ class Solution:
             if s[fast] == '1':
                 count1 += 1
                 count0 = 0
-                max1 = max(count1, max1)
+                
             elif s[fast] == '0':
                 count0 += 1
                 count1 = 0
-                max0 = max(count0, max0)
+            max1 = max(count1, max1)
+            max0 = max(count0, max0)
             fast += 1
         
         if max1 > max0:
