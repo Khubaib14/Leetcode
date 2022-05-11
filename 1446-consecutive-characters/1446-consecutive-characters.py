99ac7a9154f26a55db8bbbf68de17660cd3fb738
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-05-11 16:43:30 +0530

    Time: 66 ms (37.89%), Space: 13.9 MB (13.41%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.py
+++ 1446-consecutive-characters/1446-consecutive-characters.py
@@ -1,17 +1,15 @@
 class Solution:
     def maxPower(self, s: str) -> int:
-        slow = 0
         fast = 0
         curr = 0
         maximum = 0
-        count = 0
+        length = 0
 
         while fast < len(s):
             if ord(s[fast]) == curr:
                 length += 1
 
             elif ord(s[fast]) != curr:
-                # slow = fast
                 curr = ord(s[fast])
                 length = 0
 
