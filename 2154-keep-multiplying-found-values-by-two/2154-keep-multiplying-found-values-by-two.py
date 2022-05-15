5871346a10ae487a8f4beac63dd8d9cf87140ae8
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-05-16 00:27:09 +0530

    Time: 60 ms (97.25%), Space: 14.1 MB (63.60%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.py
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.py
@@ -1,5 +1,22 @@
 class Solution:
     def findFinalValue(self, nums: List[int], original: int) -> int:
+        while original in nums:
+            for i in nums:
+                if original == i:
+                    original *= 2
+                    break
+
+        return (original)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    def solOne(nums):
         d = {}
 
         for i in nums:
