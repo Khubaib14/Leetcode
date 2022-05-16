a6cec36b21581799070751da31c97f7b307d24e2
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-05-16 16:09:18 +0530

    Time: 40 ms (72.68%), Space: 13.9 MB (59.93%) - LeetHub

--- 747-largest-number-at-least-twice-of-others/747-largest-number-at-least-twice-of-others.py
+++ 747-largest-number-at-least-twice-of-others/747-largest-number-at-least-twice-of-others.py
@@ -1,5 +1,23 @@
 class Solution:
     def dominantIndex(self, nums: List[int]) -> int:
+        max1 = -1
+        max2 = -1
+        index = 0
+
+        for i in range(len(nums)):
+            if nums[i] > max1:
+                max2 = max1
+                max1 = nums[i]
+            elif nums[i] > max2:
+                max2 = nums[i]
+
+        if max1 >= max2*2:
+            return nums.index(max1)
+        else:
+            return -1    
+        
+        
+    def solOne(nums):
         maxindex = 0
         maxelement = 0
         secondmax = 0
