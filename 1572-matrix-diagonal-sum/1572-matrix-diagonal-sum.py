dcb1eaa51c9944f237f5db4adcefe2703bfc5bf4
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-05-19 13:06:10 +0530

    Time: 111 ms (89.24%), Space: 14.1 MB (55.97%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -1,6 +1,20 @@
 class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
         first_sum = 0
+        for i in range(len(mat[0])):
+            other = (len(mat[0])) - i - 1
+            first_sum += mat[i][i]
+            if i != other:
+                first_sum += mat[i][other]
+
+        return (first_sum)
+        
+        
+        
+        
+        
+    def solOne(mat):    
+        first_sum = 0
         second_sum = 0
         
         for i in range(len(mat[0])):
