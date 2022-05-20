ec5422c342cdae193deba15d5d7f5d29eedcb0ea
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-05-20 20:37:14 +0530

    Time: 431 ms (9.83%), Space: 16.2 MB (71.83%) - LeetHub

--- 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.py
+++ 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.py
@@ -12,6 +12,26 @@ class Solution:
                 even += 2
             elif (nums[odd] % 2 != 0) and (nums[even] % 2 != 0):
                 odd  += 2
+            else:
+                odd += 2
+                even += 2
+
+        return (nums)
+        
+        
+        
+        odd = 1
+        even = 0
+
+        while odd < len(nums) and even < len(nums):
+            if (nums[odd] % 2 == 0) and (nums[even] % 2 != 0):
+                nums[odd], nums[even] = nums[even], nums[odd]
+                odd += 2
+                even += 2
+            elif (nums[odd] % 2 == 0) and (nums[even] % 2 == 0):
+                even += 2
+            elif (nums[odd] % 2 != 0) and (nums[even] % 2 != 0):
+                odd  += 2
             elif (nums[odd] % 2 != 0) and (nums[even] % 2 == 0):
                 odd += 2
                 even += 2
