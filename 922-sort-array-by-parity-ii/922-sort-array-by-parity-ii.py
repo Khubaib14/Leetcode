8a7ca56eb7faec323dc9fb88cfd07eb1881b356c
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-05-20 20:44:52 +0530

    Time: 239 ms (65.74%), Space: 16.3 MB (71.83%) - LeetHub

--- 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.py
+++ 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.py
@@ -2,6 +2,22 @@ class Solution:
     def sortArrayByParityII(self, nums: List[int]) -> List[int]:
         odd = 1
         even = 0
+
+        while odd < len(nums) and even < len(nums):
+            if nums[odd] % 2 == 1:
+                odd += 2
+            elif nums[even] % 2 == 0:
+                even += 2
+            else: 
+                nums[odd], nums[even] = nums[even], nums[odd]
+                even += 2
+                odd += 2
+        return (nums)
+        
+        
+    def solOne(nums):
+        odd = 1
+        even = 0
         size = len(nums)
         while odd < size and even < size:
             if (nums[odd] % 2 == 0) and (nums[even] % 2 != 0):
