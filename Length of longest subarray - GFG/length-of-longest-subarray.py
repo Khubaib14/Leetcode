91c71fefef1d878180128c3b5c001854e8956c7c
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-06-02 18:35:02 +0530

    Added solution - LeetHub

---
+++ Length of longest subarray - GFG/length-of-longest-subarray.py
@@ -0,0 +1,47 @@
+#User function Template for python3
+
+def longestSubarry( arr, N):
+    slow = 0
+    fast = 0
+    l = []
+    maxer = 0
+    
+    while fast < len(arr):
+        l.append(arr[fast])
+        # maxer = max(len(l), maxer)
+        if l and l[-1] < 0:
+           while l and l[-1] < 0:
+               del l[0]
+               slow += 1
+        maxer = max(len(l), maxer)
+        fast += 1
+        
+    return (maxer)
+    
+    
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        print(longestSubarry(a, n))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+    
+# } Driver Code Ends
