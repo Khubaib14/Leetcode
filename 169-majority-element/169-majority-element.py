fac1b00ac9d69d26250785367d0c71f81fbb24d2
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-06-02 12:12:15 +0530

    Time: 299 ms (16.59%), Space: 15.6 MB (34.79%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,13 +1,22 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        element = 0
+        for i in nums:
+            if count == 0:
+                element = i
+            if element == i:
+                count += 1
+            else:
+                count -= 1
+        return element
+        
+        
+    def Sort(self, nums):
         nums.sort()
         return nums[len(nums)//2]
         
-        
-        
-        
-        
-        
+            
     def hashmap(self, nums):
         if len(nums) == 1:
             return nums[0]
