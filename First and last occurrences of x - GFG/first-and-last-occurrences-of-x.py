b6366a278dca381821ecefcfa9167b2ad1cc80d9
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-06-06 14:26:49 +0530

    Added solution - LeetHub

--- First and last occurrences of x - GFG/first-and-last-occurrences-of-x.py
+++ First and last occurrences of x - GFG/first-and-last-occurrences-of-x.py
@@ -1,8 +1,62 @@
 #User function Template for python3
 
+def BinarySearchFirst(arr, k):
+    start = 0
+    end = len(arr) - 1
+    mid = (start + end)//2
+    
+    first = -1
+    
+    while start <=  end:
+        if arr[mid] == k:
+            first = mid
+            end = mid - 1
+            
+        elif arr[mid] < k:
+            start = mid + 1
+        else:
+            end = mid - 1 
+        
+        mid = (start + end)//2
+    
+    return first
+    
+
+def BinarySearchLast(arr, k):
+    start = 0
+    end = len(arr) - 1
+    mid = (start + end)//2
+    
+    last = -1
+    
+    while start <=  end:
+        if arr[mid] == k:
+            last = mid
+            start = mid + 1
+            
+        elif arr[mid] < k:
+            start = mid + 1
+        else:
+            end = mid - 1 
+        
+        mid = (start + end)//2
+    
+    return last
+
 
 def find(arr,n,x):
     # code here
+    first = BinarySearchFirst(arr, x)
+    last = BinarySearchLast(arr, x)
+    return first, last
+    
+    
+    
+    
+    
+
+#######################    
+def solOne(arr,n,x):    
     a = []
     for i in range(n):
        if arr[i]==x:
