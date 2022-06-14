367a36d4d6703171b03030dde5134fe26c27583f
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-06-14 10:58:37 +0530

    Time: 81 ms (27.75%), Space: 14 MB (17.89%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
@@ -3,38 +3,48 @@ class Solution:
         start = 0
         end = len(arr) - 1
         mid = (start+end)//2
-        result = []
+        ans = -1
         while start <= end:
             if arr[mid] == key:
-                result.append(mid)
+                ans = mid
                 end = mid - 1
             elif arr[mid] > key:
                 end = mid - 1
             else:
                 start = mid + 1
             mid = (start + end)//2
-        return result
+        return ans
     
     
     def SearchRight(self, arr, key):
         start = 0
         end = len(arr) - 1
         mid = (start+end)//2
-        result = []
+        ans = -1
         while start <= end:
             if arr[mid] == key:
-                result.append(mid)
+                ans = mid
                 start = mid + 1
             elif arr[mid] > key:
                 end = mid - 1
             else:
                 start = mid + 1
             mid = (start + end)//2
-        return result
+        return ans
     
     
     
     def targetIndices(self, arr: List[int], key: int) -> List[int]:
+        arr.sort()
+        right = self.SearchRight(arr, key) 
+        left = self.SearchLeft(arr, key)
+        if right == -1 or left == -1:
+            return []
+        return [i for i in range(left, right+1)]
+        
+        
+        
+    def SolTwo(self, arr, key):
         lt_count, eq_count = 0, 0
         for n in arr:
             if n < key:
@@ -43,11 +53,3 @@ class Solution:
                 eq_count += 1
             
         return list(range(lt_count, lt_count+eq_count))
-        
-        
-        
-        
-        #arr.sort()
-        #l = list(set(self.SearchRight(arr, key) + self.SearchLeft(arr, key)))
-        #l.sort()
-        #return l
