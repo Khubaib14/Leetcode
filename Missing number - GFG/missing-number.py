79d98eeb3bc53fde6bd4f33ec6a788db2604d1aa
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-06-17 17:19:31 +0530

    Added solution - LeetHub

---
+++ Missing number - GFG/missing-number.py
@@ -0,0 +1,32 @@
+#User function Template for python3
+
+def missingNumber( A, N):
+     # Your code goes here
+    theory = (N * (N + 1))//2
+    actual = sum(A)
+    return abs(theory - actual)
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        print(missingNumber(a, n))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+    
+# } Driver Code Ends
