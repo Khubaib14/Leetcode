13438ecc3c5eda58c707fac6857f41cc0f510676
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-06-20 11:21:30 +0530

    Time: 98 ms (63.36%), Space: 15.5 MB (18.99%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
@@ -2,34 +2,10 @@ class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
         start = 0
         end = len(arr) - 1
-
         mid = (start + end)//2
 
         while start <= end:
-            print(mid, arr[mid])
-            if arr[mid+1] < arr[mid] and arr[mid-1] < arr[mid]:
-                return mid
-            elif arr[mid] < arr[mid+1]:
-                start = mid 
-            elif arr[mid-1] > arr[mid]:
-                end = mid 
-
-            mid = (start + end)//2
-
-        return -1
-        
-        
-        
-        
-        
-    def solOne(self, arr):
-        start = 0
-        end = len(arr) - 1
-
-        mid = (start + end)//2
-
-        while start <= end:
-            print(mid, arr[mid])
+            # print(mid, arr[mid])
             if arr[mid+1] < arr[mid] and arr[mid-1] < arr[mid]:
                 return mid
             elif arr[mid-1] < arr[mid] < arr[mid+1]:
