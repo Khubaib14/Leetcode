0838cb7727f8269d75637a1e5021c8476a003007
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-06-23 13:44:14 +0530

    Time: 62 ms (10.08%), Space: 13.9 MB (8.81%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
@@ -1,5 +1,20 @@
 class Solution:
+    def Solve(self, n, cnt):
+        if n == 0:
+            return cnt
+        if n % 2 == 0:
+            return self.Solve(n//2, cnt+1)
+        else:
+            return self.Solve(n-1, cnt+1)
+    
+    
     def numberOfSteps(self, num: int) -> int:
+        cnt = 0
+        return self.Solve(num, cnt)
+        
+ 
+    def solOne(self, num):
+        # First attempt
         count = 0
         while num > 0:
             if num % 2 == 0:
