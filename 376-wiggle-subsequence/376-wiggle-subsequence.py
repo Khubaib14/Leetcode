93967ac6c8bb5e59fc3a3ea4b97d0313d3834fb7
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-07-03 16:54:45 +0530

    Time: 47 ms (68.28%), Space: 14 MB (10.82%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.py
+++ 376-wiggle-subsequence/376-wiggle-subsequence.py
@@ -1,14 +1,14 @@
 class Solution:
     def wiggleMaxLength(self, nums: List[int]) -> int:
-        flag = None
+        rise = None
         total = 1
 
         for i in range(1, len(nums)):
-            if nums[i-1] < nums[i] and flag != True:
+            if nums[i-1] < nums[i] and rise != True:
                 total += 1
-                flag = True
-            elif nums[i-1] > nums[i] and flag != False:
+                rise = True
+            elif nums[i-1] > nums[i] and rise != False:
                 total += 1
-                flag = False
+                rise = False
 
         return total
