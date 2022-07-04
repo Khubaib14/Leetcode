e02fb8eafe9deaed2f0b80ecf937233d574308bf
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-07-05 00:33:49 +0530

    Time: 41 ms (94.11%), Space: 14.1 MB (31.23%) - LeetHub

--- 682-baseball-game/682-baseball-game.py
+++ 682-baseball-game/682-baseball-game.py
@@ -1,5 +1,31 @@
 class Solution:
     def calPoints(self, ops: List[str]) -> int:
+        stk = []
+        res = 0
+        
+        for op in ops:
+            if op == 'C':
+                res -= stk.pop()
+            elif op == 'D':
+                stk.append(stk[-1]*2)
+                res += stk[-1]
+            elif op == '+':
+                stk.append(stk[-1]+stk[-2])
+                res += stk[-1]
+            else:
+                stk.append(int(op))
+                res += stk[-1]
+                    
+        return res
+        
+        
+        
+        
+        
+        
+        
+        
+    def solOne(self, ops):
         history = []
         for op in ops:
             if op == 'C':
