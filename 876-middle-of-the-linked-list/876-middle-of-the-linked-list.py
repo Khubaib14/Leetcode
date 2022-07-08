3dfcab425258819defc022d851a387236941afb5
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-07-08 15:44:40 +0530

    Time: 66 ms (8.48%), Space: 14 MB (10.90%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
@@ -5,20 +5,29 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+        while fast and fast.next:
+            # while checks if fast is not
+            # None and the next Node is 
+            # also not None
+            slow = slow.next
+            fast = (fast.next).next
+        return slow
+        
+        
+        
+    def solOne(self):
         length = 0
         temp = head
         while head:
             length += 1
             head = head.next
-        
         cnt = 0
         start_from = length//2
-        
         while temp:
             if cnt == start_from:
                 break
-        
             cnt += 1
             temp = temp.next
-        
         return temp
