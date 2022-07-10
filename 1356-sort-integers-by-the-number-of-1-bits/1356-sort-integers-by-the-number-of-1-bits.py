3fb28df7374bee5ac94b9a20c8a85b69ddeb1777
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-07-10 15:38:19 +0530

    Time: 119 ms (40.50%), Space: 14.1 MB (78.45%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
@@ -1,3 +1,9 @@
 class Solution:
     def sortByBits(self, arr: List[int]) -> List[int]:
+        return sorted(arr, key = lambda num : (sum((num >> i) & 1 for i in range(32)), num))
+        
+        
+        
+        
+    def solOne(arr):
         return sorted(arr, key=lambda x: (bin(x).count('1'), x))
