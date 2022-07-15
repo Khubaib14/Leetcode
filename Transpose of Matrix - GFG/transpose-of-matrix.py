1ae793df637c2bc78a66cf70380a79dd67cedc85
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-07-15 17:02:53 +0530

    Added solution - LeetHub

---
+++ Transpose of Matrix - GFG/transpose-of-matrix.py
@@ -0,0 +1,38 @@
+#Back-end complete function Template for Python 3
+
+class Solution:
+    
+    #Function to find transpose of a matrix.
+    def transpose(self,matrix, n):
+        for i in range(len(matrix)):
+            j = i
+            while j < len(matrix):
+                if j != i:
+                    matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+                j += 1
+        return matrix
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        n = int(input())
+        matrix = [[0 for j in range(n)] for i in range(n)]
+        line1 = [int(x) for x in input().strip().split()]
+        k=0
+        for i in range(n):
+            for j in range (n):
+                matrix[i][j]=line1[k]
+                k+=1
+        obj = Solution()
+        obj.transpose(matrix,n)
+        for i in range(n):
+            for j in range(n):
+                print(matrix[i][j],end=" ")
+        print()
+            
+
+# } Driver Code Ends
