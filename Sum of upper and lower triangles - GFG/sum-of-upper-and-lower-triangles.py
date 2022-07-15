a171bf460b101bbb5bcc6d1f2b5751866f53234f
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-07-15 12:32:06 +0530

    Added solution - LeetHub

---
+++ Sum of upper and lower triangles - GFG/sum-of-upper-and-lower-triangles.py
@@ -0,0 +1,46 @@
+
+#User function Template for python3
+
+
+class Solution:
+    
+    #Function to return sum of upper and lower triangles of a matrix.
+    def sumTriangles(self,matrix, n):
+        # code here 
+        upper = 0
+        lower = 0
+        for i in range(len(matrix)):
+            j = i
+            while j >= i and j < len(matrix):
+                #print(matrix[i][j], end = " ")
+                upper += matrix[i][j]
+                j += 1
+        for i in range(len(matrix)):
+            j = 0
+            while j <= i:
+                lower += matrix[i][j]
+                j += 1
+        return (upper, lower)
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        matrix = [[0 for j in range(n)] for i in range(n)]
+        line1 = [int(x) for x in input().strip().split()]
+
+        k=0
+        for i in range(n):
+            for j in range (n):
+                matrix[i][j]=line1[k]
+                k+=1
+        obj = Solution()
+        ans = obj.sumTriangles(matrix, n)
+        for i in ans:
+            print(i,end=" ")
+        print()
+# } Driver Code Ends
