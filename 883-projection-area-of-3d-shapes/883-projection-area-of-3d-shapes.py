bed1a19df9a6dc1ece0ac19ba300e02ab57d5bc3
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-07-26 17:38:06 +0530

    Time: 96 ms (68.68%), Space: 13.9 MB (92.86%) - LeetHub

--- 883-projection-area-of-3d-shapes/883-projection-area-of-3d-shapes.py
+++ 883-projection-area-of-3d-shapes/883-projection-area-of-3d-shapes.py
@@ -1,18 +1,10 @@
 class Solution:
     def projectionArea(self, grid: List[List[int]]) -> int:
         total = 0
-        
         # top
-        cnt = 0
-        c = len(grid[0])
-        for i in range(c*c):
-            if grid[i//c][i%c] == 0:
-                cnt += 1
-        total += (len(grid)*len(grid[0]) - cnt)
-        
+        total += sum([1 for i in grid for j in i if j > 0])
         # front
         total +=  sum([max(col) for col in zip(*grid)])
-        
         # side
         total +=  sum([max(row) for row in grid])
         return total
