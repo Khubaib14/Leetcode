a9b4b91802190fdefff23b633558015f2f590bb8
Khubaib <72076919+Khubaib14@users.noreply.github.com>
2022-07-27 16:40:18 +0530

    Time: 165 ms (97.59%), Space: 14.2 MB (86.51%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.py
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.py
@@ -3,10 +3,18 @@ class Solution:
         rows = []
         cols = []
         cnt = 0
+        
+        # sum of row = 1 means only one 1
         for i in mat:
             rows.append(sum(i))
+        
+        # sum of col = 1 means only one 1
         for i in zip(*mat):
             cols.append(sum(i))
+        
+        # for pos where 1 is present 
+        # see if that pos's rowsum and 
+        # colsum is also 1
         for i in range(len(mat)):
             for j in range(len(mat[0])):
                 if rows[i] == cols[j] == mat[i][j] == 1:
